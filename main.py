5c7ec2c8eeabceb850b6ade7232a6657e681920d
okano0120 <okano0120@gmail.com>
2022-07-16 15:03:30 +0900

    add selenium

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-import requests
+# import requests
+import inspect
 import json
+# from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-livemoni = requests.get('https://www.supertaikyu.live/json/livemoni.json')
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-# print(['LiveData'])
-# data = json.loads(livemoni.json())
-print(json.dumps(livemoni.json(), indent=2))
+driver.get("https://www.supertaikyu.live/timings/");
+
+# element = driver.find_element_by_css_selector('#timing_table')
+
+time.sleep(3)
+t = driver.find_element("id", "timing_table")
+print(t)
+
+# print(type(driver))
+# for x in inspect.getmembers(driver, inspect.ismethod):
+#   print(x[0])
+# # print(element)
+
+# html_text = requests.get('https://www.supertaikyu.live/timings/').text
+# soup = BeautifulSoup(html_text, 'html.parser')
+
+# print(soup.select("#timing_table")	)
