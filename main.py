7d970bcd26654bf6918dc0cf03afc95ed093199b
okano0120 <okano0120@gmail.com>
2022-07-16 16:16:19 +0900

    add main

---
+++ main.py
@@ -0,0 +1,13 @@
+from settings import Settings
+from settings import FetcherType
+
+# print(Settings.interval_second)
+
+class JSONController:
+  @classmethod
+  def exec(self):
+    print('exec')
+
+# JSONで取得してくる方法
+JSONController.exec()
+
