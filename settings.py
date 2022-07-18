b24dac7e1847547111cacd10e21cd6ea1a88ce33
okano0120 <okano0120@gmail.com>
2022-07-18 16:47:47 +0900

    change setting

--- settings.py
+++ settings.py
@@ -6,14 +6,22 @@ class FetcherType(Enum):
   TEST = 3
 
 class Settings:
-  # 何秒間隔でリクエストを行うのか
+  # === COMMON ===
+  # 何秒間隔でリクエストを行うのか(秒)
   interval_second = 10
 
-  # Json APIのSect2SectTimeの順番がどうなっているのか [only JSON Fetcher]
-  sect2_order = [3, 0, 1, 2]
+  # 車のID (String)
+  car_number = '244'
 
-  # テーブルで必要な値 n番目, 0番目始まり [only HTML Fetcher]
-  table_order = [4, 12, 13, 14, 15]
+  # === JSON Fetcher ===
+  # Json APIのSect2SectTimeの順番がどうなっているのか
+  sect2_orders = [3, 0, 1, 2]
 
+  # === HTML Fetcher ===
+  # テーブルで必要な値 n番目, 0番目始まり
+  car_number_order = 2 # TODO: 正しく
+  table_orders = [4, 12, 13, 14, 15]
+
+  # === NOT CHANGE ===
   # 取得方法は何にするのか [手動で変更しない]
   fetcher_type = FetcherType.HTML
